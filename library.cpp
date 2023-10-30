87cab5e8fb484c75dfd7b02480384cbadb0827a0
Justinfinn2891 <jfinn5@ut.utm.edu>
2023-10-30 16:47:36 -0500

    yomam

--- library.cpp
+++ library.cpp
@@ -18,11 +18,24 @@ void library::print()
 
 void library::readFromFile()
 {
+  book temp; 
   ifstream file; 
   cout << "Insert file name: ";
   cin >> filename;
   file.open(filename + ".txt");
-
+  getline(file, temp.book_title, ',');
+  getline(file, temp.author_name., ',');
+  cin >> temp.isbn_code >> temp.page_num >> temp.cover_price;
   
+  while(file)
+    {
+      insert_sorted(temp.book_title, temp.author_name, temp.isbn_code, temp.page_num, temp.cover_price);
+      getline(file, temp.book_title, ',');
+      getline(file, temp.author_name, ',');
+      cin >> temp.isbn_code >> temp.page_num >> temp.cover_price; 
+    }
+  cout << "===================================" << endl;
+  cout << "====FILE ENTERED SUCCESSFULLY======" << endl;
+  cout << "===================================" << endl; 
 }
 
--- library.h
+++ library.h
@@ -20,7 +20,6 @@ public:
 
   void insertSorted(std::string book_title, std::string author_name, std::string isbn_code, int page_num, int year_num, float cover_price);
   void lookup();
-
   
   void reverse_lookup();
   void print();
