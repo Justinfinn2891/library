8b06b814e215de476d8cf516d7ea05c498722d3b
Justinfinn2891 <jfinn5@ut.utm.edu>
2023-10-30 16:52:02 -0500

    errors

--- library.cpp
+++ library.cpp
@@ -18,21 +18,22 @@ void library::print()
 
 void library::readFromFile()
 {
+  string filename;
   book temp; 
   ifstream file; 
   cout << "Insert file name: ";
   cin >> filename;
   file.open(filename + ".txt");
   getline(file, temp.book_title, ',');
-  getline(file, temp.author_name., ',');
-  cin >> temp.isbn_code >> temp.page_num >> temp.cover_price;
+  getline(file, temp.author_name, ',');
+  cin >> temp.isbn_code >> temp.page_num >> temp.year_num >> temp.cover_price;
   
   while(file)
     {
-      insert_sorted(temp.book_title, temp.author_name, temp.isbn_code, temp.page_num, temp.cover_price);
+      insertSorted(temp.book_title, temp.author_name, temp.isbn_code, temp.page_num, temp.year_num, temp.cover_price);
       getline(file, temp.book_title, ',');
       getline(file, temp.author_name, ',');
-      cin >> temp.isbn_code >> temp.page_num >> temp.cover_price; 
+      cin >> temp.isbn_code >> temp.page_num >> temp.year_num >> temp.cover_price; 
     }
   cout << "===================================" << endl;
   cout << "====FILE ENTERED SUCCESSFULLY======" << endl;
